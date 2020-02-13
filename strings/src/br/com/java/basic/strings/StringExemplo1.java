7aecd85f701e33e7bf1dde7aa37b04cd71d30105
silascaimi <silascaimi@live.com>
2020-02-13 09:55:07 -0300

    Adicione exemplos de String e StringBuilder

---
+++ strings/src/br/com/java/basic/strings/StringExemplo1.java
@@ -0,0 +1,25 @@
+package br.com.java.basic.strings;
+
+public class StringExemplo1 {
+
+	public static void main(String[] args) {
+
+		String s1 = new String(); // cria novo objeto da classe String
+		String s2 = "abc"; // cria uma string
+		String s3 = "abc";
+		String s4 = new String("abc");
+
+		s1 = s2.toUpperCase();
+
+		System.out.println(s1);
+
+		System.out.println(s1.concat(s2));
+
+		System.out.println(s2.equals(s3));
+		System.out.println(s2 == s3);
+		System.out.println(s2.equals(s4));
+		System.out.println(s2 == s4);
+
+	}
+
+}
---
+++ strings/src/br/com/java/basic/strings/StringExemplo2.java
@@ -0,0 +1,27 @@
+package br.com.java.basic.strings;
+
+public class StringExemplo2 {
+
+	public static void main(String[] args) {
+
+		String s1 = "Pedro";
+		String s2 = " Pedro   C";
+
+		System.out.println(s1.equalsIgnoreCase(s2));
+
+		System.out.println(s1.charAt(0));
+
+		// Remove caracteres em branco antes e depois da String
+		System.out.println(s2.trim());
+
+		// substring(int beginIndex);
+		// substring(int beginIndex, int endIndex);
+
+		System.out.println(s1.indexOf('r'));
+
+		String curso = "Curso_de_desenvolvimento";
+		System.out.println(curso.replaceAll("_", " "));
+
+	}
+
+}
