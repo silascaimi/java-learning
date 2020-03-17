fb50164013fea00548a1b4e4af186a189c30665a
silascaimi <silascaimi@live.com>
2020-03-17 12:05:40 -0300

    Aplicar conversão customizada

---
+++ jsf/conversor-customizado/src/JavaBean.java
@@ -0,0 +1,28 @@
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import com.javaee.jsf.model.Endereco;
+
+@Named
+@RequestScoped
+public class JavaBean {
+
+	Endereco endereco;
+	
+	public String processar() {
+		System.out.println("Rua: " + endereco.getRua());
+		System.out.println("Número: " + endereco.getNumero());
+		
+		return "result";
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
---
+++ jsf/conversor-customizado/src/com/javaee/jsf/converter/EndereçoConverter.java
@@ -0,0 +1,33 @@
+package com.javaee.jsf.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import com.javaee.jsf.model.Endereco;;
+
+@FacesConverter(forClass = Endereco.class)
+public class EndereçoConverter implements Converter<Object> {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent ui, String value) {
+		if (value == null) {
+			return null;
+		}
+
+		String[] tokens = value.split(",");
+		Endereco endereco = new Endereco();
+		endereco.setRua(tokens[0].trim());
+		endereco.setNumero(tokens[1].trim());
+
+		return endereco;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent ui, Object value) {
+		Endereco endereco = (Endereco) value;
+		return endereco.getRua() + ", " + endereco.getNumero();
+	}
+
+}
