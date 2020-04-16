c9b914e1eb5f9a9813c4a258c23d236dc9bf3b52
silascaimi <silascaimi@live.com>
2020-04-16 12:22:22 -0300

    Implementar JOptionPane

---
+++ basic/JOptionPane/src/ExemploJOptionPane3.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class ExemploJOptionPane3 {
+
+	public static void main(String[] args) {
+
+		String[] opcoes = {"Masculino", "Feminino"};
+		String sexo = (String) JOptionPane.showInputDialog(null, "Selecione o sexo", "AlgaWorks", 
+				JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
+		
+		System.out.println("Sexo selecionado: " + sexo);
+		
+		String nome = (String) JOptionPane.showInputDialog(null, "Digite o seu nome", "AlgaWorks", JOptionPane.PLAIN_MESSAGE, null, null, null);
+		
+		System.out.println("Seu nome é: " + nome);
+	}
+
+}
