e12ed458c0e25e46abdfc5aca45e17157b978e54
silascaimi <silascaimi@live.com>
2020-04-28 17:06:55 -0300

    Implementar paginas web

---
+++ advanced/maven-webapp/src/main/java/com/webapp/UserListServlet.java
@@ -0,0 +1,51 @@
+package com.webapp;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webapp.model.User;
+
+/**
+ * Servlet implementation class UserListServlet
+ */
+@WebServlet("/user-list")
+public class UserListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserListServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		User mary = new User("Mary", 33);
+		User james = new User("James", 26);
+		User patricia = new User("Patricia", 28);
+		User john = new User("John", 21);
+		
+		List<User> users = Arrays.asList(mary, james, patricia, john);
+		request.setAttribute("userList", users);
+		request.getRequestDispatcher("users-list.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
